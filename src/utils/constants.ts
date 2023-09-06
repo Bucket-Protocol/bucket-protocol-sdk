4d1a45f133364c0d3cc4f2d99671fac81b2379a9
andreidev1 <d.andrei01@yahoo.com>
2023-09-06 16:47:20 +0300

    Add MAINNET constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,11 @@
 // Copyright Andrei <andreid.dev@gmail.com>
 
+export const MAINNET_PACKAGE_ID: string = 
+  "0xce7ff77a83ea0cb6fd39bd8748e2ec89a3f41e8efdc3f4eb123e0ca37b184db2";
+
+export const MAINNET_BUCKET_OPERATIONS_PACKAGE_ID: string =
+  "0x6f206ba15a7d81662e20ac1e6a4e0b443f3972861327584e8a1148c9880e4a09";
+
 export const TESTNET_PACKAGE_ID: string =
   "0x1ca47988f33d06d748a779e78f321b9ba74f6ad25b3de2840da425022dfaa969";
 
