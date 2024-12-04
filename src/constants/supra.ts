78d5afb6a3d5e6e7213d650e2366e2e8636f9d21
justaliang <justaliang@protonmail.com>
2024-12-04 15:57:12 +0800

    add SBETH supra price index

--- src/constants/supra.ts
+++ src/constants/supra.ts
@@ -9,16 +9,26 @@ export const SUPRA_HANDLER_OBJECT = {
 };
 
 export const SUPRA_PRICE_FEEDS = {
-  usdc_usd: "0x1c400c096e8b52a22c43c080fea4aa22661c9a35b469493dfac5332aecb4789c",
-  usdt_usd: "0x11ea8c7b6287f1410c8bac2e475b4fe6fea45fd59e036a058522ab3acec8fed3",
-  usdy_usd: "0x95d93c5bed776b020ce848964572d90bf4694a73fd06c7238a4be05bf438d724",
-  eth_usdt: "0x258d0f9c61768d184d218a068ff8117728f5becb1bfe2b1e3b6e231dda875a67",
-  sui_usdt: "0x898236b8f93d5ea7f94fbec1b3ac152ae51901e3078859a92cbf06c46c20b316",
-  hasui_sui: "0xd741ca15eb5e19f2d138b9e9cb95d70dc90667370dee7cbad184acfbb35063cf",
-  vsui_sui: "0xd291ea0aebb1a9314cd60d5142755267f9badbeccd30b91e92b54bef29864f33",
-  afsui_sui: "0xbe4289cf638a9ad1f085bd8a3e0aeaed814078e3961b77fb08cc7550ee5d8c00",
-  navx_usd: "0x996cfab93ecd365cea4e2ea5bb6d29160192513eec5e08521d0edad3796f667c",
-  cetus_usd: "0xc0205c51048f09dee3d28c08d1f3dd8c8b5aca65337aac63562e8b8140b32ac6",
+  usdc_usd:
+    "0x1c400c096e8b52a22c43c080fea4aa22661c9a35b469493dfac5332aecb4789c",
+  usdt_usd:
+    "0x11ea8c7b6287f1410c8bac2e475b4fe6fea45fd59e036a058522ab3acec8fed3",
+  usdy_usd:
+    "0x95d93c5bed776b020ce848964572d90bf4694a73fd06c7238a4be05bf438d724",
+  eth_usdt:
+    "0x258d0f9c61768d184d218a068ff8117728f5becb1bfe2b1e3b6e231dda875a67",
+  sui_usdt:
+    "0x898236b8f93d5ea7f94fbec1b3ac152ae51901e3078859a92cbf06c46c20b316",
+  hasui_sui:
+    "0xd741ca15eb5e19f2d138b9e9cb95d70dc90667370dee7cbad184acfbb35063cf",
+  vsui_sui:
+    "0xd291ea0aebb1a9314cd60d5142755267f9badbeccd30b91e92b54bef29864f33",
+  afsui_sui:
+    "0xbe4289cf638a9ad1f085bd8a3e0aeaed814078e3961b77fb08cc7550ee5d8c00",
+  navx_usd:
+    "0x996cfab93ecd365cea4e2ea5bb6d29160192513eec5e08521d0edad3796f667c",
+  cetus_usd:
+    "0xc0205c51048f09dee3d28c08d1f3dd8c8b5aca65337aac63562e8b8140b32ac6",
   sca_usd: "0x94dff56ca222ecb8b1df97596720e1d5de65e31e242067f19c8ef9544050233c",
 };
 
@@ -36,4 +46,5 @@ export const SUPRA_ID: Record<string, number> = {
   NAVX: 408,
   CETUS: 93,
   SCA: 476,
+  SBETH: 19,
 };
