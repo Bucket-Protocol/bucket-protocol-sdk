597c8cca3293093e3eb5e232c657e01bac8e82d9
justaliang <justaliang@protonmail.com>
2025-03-04 06:45:23 -0700

    upgrade scable package ID

--- src/client.ts
+++ src/client.ts
@@ -3116,7 +3116,7 @@ export class BucketClient {
       });
       return tx.moveCall({
         target:
-          "0x1b4b5dd782453586f2a39dbc154f9ef4ca7780ae9a31154c1c6080985a5d295e::scable::withdraw_coin_from_spool",
+          "0x3eca32f66d6269d0a6c400a760182df74330a4cbf61548f37892c680b295b085::scable::withdraw_coin_from_spool",
         typeArguments: [outCoinType],
         arguments: [
           tx.sharedObjectRef(vaultObj),
