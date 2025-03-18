7c6d2ba7204b339491a158981897a2a2f49225ff
zasper32171 <zasper32171@gmail.com>
2025-03-18 22:27:12 +0800

    fix: pull submodule

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,21 +2,41 @@
 
 echo "setting up previews..."
 
-pwd=$(pwd)
-branch=$(git branch --show-current)
+ROOT_DIR=$(pwd)
+CURRENT_BRANCH=$(git branch --show-current)
 
-for repo in previews/*; do
-  echo "setting up previews for repo: $repo..."
+function sync_submodules {
+  echo "pull submodules:"
 
-  cd $pwd/$repo
-  git fetch --all
+  sed -i'.bak' "s/https:\/\/github.com\//https:\/\/${GITHUB_TOKEN}@github.com\//" "$ROOT_DIR/.gitmodules"
 
-  if git show-ref --quiet origin/$branch; then
-    target=origin/$branch
-  else
-    target=$(git symbolic-ref --short refs/remotes/origin/HEAD) 
-  fi
+  git submodule update --remote
+}
 
-  echo "set to branch $target"
-  git checkout -f $target
-done
+function checkout_branches {
+  echo "checkout branches for each preview repos:"
+
+  for repo in previews/*; do
+    echo "  setting up previews for repo $repo:"
+
+    cd $ROOT_DIR/$repo
+    git fetch --all
+
+    if git show-ref --quiet origin/$branch; then
+      target=origin/$CURRENT_BRANCH
+    else
+      target=$(git symbolic-ref --short refs/remotes/origin/HEAD) 
+    fi
+
+    echo "    set to branch $target"
+    git checkout -f $target
+  done
+}
+
+if [ -z $GITHUB_TOKEN ]; then
+  echo '$GITHUB_TOKEN is not provided. Abort.'
+  exit 1
+fi
+  
+sync_submodules
+checkout_branches
