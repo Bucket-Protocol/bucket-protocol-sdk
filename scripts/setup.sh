03b613b1fa1072bb502ec42fd7131086c68522f6
zasper32171 <zasper32171@gmail.com>
2025-03-27 15:28:33 +0800

    test: output branch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,6 +27,9 @@ function checkout_branches {
     cd $ROOT_DIR/$repo
     git fetch --all
 
+    echo $(git show-ref origin/$CURRENT_BRANCH)
+    git branch -a | cat
+
     if git show-ref --quiet origin/$CURRENT_BRANCH; then
       target=origin/$CURRENT_BRANCH
     else
