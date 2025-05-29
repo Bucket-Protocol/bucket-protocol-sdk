25165c3d326e27bc6aff963fbf5a5cc15a30e378
zasper32171 <zasper32171@gmail.com>
2025-05-29 22:27:10 +0800

    remvoe test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-import { BucketClient } from './sdk/dist/index.js';
-
-const bucketClient = new BucketClient();
-const positions = await bucketClient.getUserBottles(
-  '0x95b0ce9775382b88a4e698d31a0a7fd796922c91bb80de66e940bd4cae5a9916',
-);
-console.log(positions);
