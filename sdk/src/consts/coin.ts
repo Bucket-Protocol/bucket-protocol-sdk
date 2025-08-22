33fe250f328f19cd315649c2202d8ab9a04832f2
Jarek Lin <s410673074@gmail.com>
2025-08-22 11:30:58 +0800

    feat: add USDB coin type

--- sdk/src/consts/coin.ts
+++ sdk/src/consts/coin.ts
@@ -1,4 +1,5 @@
-export type COIN = 'USDC';
+export type COIN = 'USDC' | 'USDB';
 export const COIN_TYPES: Record<COIN, string> = {
   USDC: '0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC',
+  USDB: '0x5eb92323ce3148b222cbf035804078ff52577f414cc7abcd4e20a1243e9907f9::usdb::USDB',
 };
