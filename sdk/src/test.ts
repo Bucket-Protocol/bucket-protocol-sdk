074e99e01305ea5f948beeb4d06db4102acdecaa
zasper32171 <zasper32171@gmail.com>
2025-09-10 22:22:01 +0800

    remove test.ts

--- sdk/src/test.ts
+++
@@ -1,4 +0,0 @@
-import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
-
-const suiClient = new SuiClient({ url: getFullnodeUrl('mainnet') });
-const bucketClient = new BucketClient({ suiClient });
